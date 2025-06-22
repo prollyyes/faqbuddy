d9c43c9e7dc8bb418b6e86c4da54113bb52f0d64
antonio <antonioponti@MacBook-Air-di-Antonio.local>
2025-06-22 18:47:42 +0200

    Stats imporoved + altre migliorie

--- frontend/src/app/profile/layout.js
+++ frontend/src/app/profile/layout.js
@@ -1,13 +1,10 @@
 import ProfileNavBar from '@/components/utils/ProfileNavBar';
 import Header from '@/components/utils/Header';
-import Link from 'next/link';
-import Button from '@/components/utils/Button';
-import { IoReturnUpBackOutline } from "react-icons/io5";
 import BackButton from '@/components/utils/BackButton';
 
 export default function ProfileLayout({ children }) {
   return (
-    <div className="min-h-screen bg-white">
+    <div className="min-h-screen bg-white ">
       <Header />
       <div className="fixed pt-24 pl-4">
         <BackButton href="/homepage/chat" />
