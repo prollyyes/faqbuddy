07e8da89e067a4d704705efcec6c821d29432c4d
LucaTamSapienza <tutordimatematica.ing@gmail.com>
2025-06-26 16:52:56 +0200

    refactored test_rag.py inside tests/ && normalized with NAMESPACE and get_connection function

--- backend/src/rag/embed_and_index.py
+++ backend/src/rag/embed_and_index.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 from src.rag.utils.embeddings import embed_texts
-from src.rag.utils.vector_db import upsert_vectors
+from src.rag.utils.vector_db import upsert_vectors, NAMESPACE
 from src.rag.utils.generate_chunks import get_chunks
 
 load_dotenv()
@@ -22,8 +22,8 @@ def embed_and_index():
         for i, (vec, text) in enumerate(zip(embeddings, chunks))
     ]
 
-    upsert_vectors(items_to_upsert, namespace="v4") # before v4, use v2
-    print("✅ Chunks indexed in Pinecone (namespace='v4').")
+    upsert_vectors(items_to_upsert, namespace=NAMESPACE)
+    print(f"✅ Chunks indexed in Pinecone (namespace='{NAMESPACE}').")
 
 if __name__ == "__main__":
     embed_and_index()
