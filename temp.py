5c20cf1e88b9e86ecce11060dee033acde3907cd
LucaTamSapienza <tutordimatematica.ing@gmail.com>
2025-06-26 15:58:28 +0200

    useless files for Fine Tuning

--- temp.py
+++
@@ -1,6 +0,0 @@
-
-from transformers import AutoTokenizer, AutoModelForCausalLM
-
-model_name = "mistralai/Mistral-7B-v0.1"
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForCausalLM.from_pretrained(model_name, device_map="auto", trust_remote_code=True)
