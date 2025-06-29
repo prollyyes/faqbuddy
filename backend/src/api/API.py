190a1def20aa86007e65769b20ef50a4d66824f2
LucaTamSapienza <tutordimatematica.ing@gmail.com>
2025-06-30 00:38:19 +0200

    now allow origins *

--- backend/src/api/API.py
+++ backend/src/api/API.py
@@ -8,7 +8,7 @@ app = FastAPI(title="api")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  
+    allow_origins=["*"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
